bab640d34ad9255974a232832a7dbe8a79eaa2f0
oscarho2 <oscar.ho@stu.gsmd.ac.uk>
2025-10-02 20:02:10 -0400

    feat: add start:prod script for deployment

--- backend/server.js
+++ backend/server.js
@@ -82,6 +82,15 @@ app.use('/api/venues', venuesRoutes);
 app.use('/api/contact', contactRoutes);
 app.use('/api/locations', locationRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*_*, (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 // DB Health endpoint
 app.get('/api/health/db', async (req, res) => {
   try {
