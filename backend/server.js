9eae26ee92951383c6ac9808891f0e12d9093291
oscarho2 <oscar.ho@stu.gsmd.ac.uk>
2025-10-02 21:30:00 -0400

    fix: simplify root package.json and fix server syntax error

--- backend/server.js
+++ backend/server.js
@@ -87,7 +87,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*_*, (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
